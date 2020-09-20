4b61521272b7f695edc24c3d4e5e0f6292dd3b95
Jared <jaredarodriguez@gmail.com>
2020-09-20 13:07:37 -0400

    logs API data

--- script.js
+++ script.js
@@ -16,10 +16,18 @@ const openSlide = document.getElementById("open-slide");
 const closeSlide = document.getElementById("close-slide");
 
 // Get Foursquare API Data
-async function getData(categoryId) {
-    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
-    const apiURL = `https://api.foursquare.com/v2/venues/search?ll=${config.LOCATION}&categoryId=${categoryId}&client_id=${config.CLIENT_ID}&client_secret=${config.CLIENT_SECRET}&v=20200920`;
+async function getData() {
+    const apiURL = `https://api.foursquare.com/v2/venues/search?ll=${config.LOCATION}&categoryId=4bf58dd8d48988d110941735&client_id=${config.CLIENT_ID}&client_secret=${config.CLIENT_SECRET}&v=20200920`;
+
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
+getData();
 // Events
 function openSlideMenu() {
   document.getElementById("side-menu").style.width = "250px";
